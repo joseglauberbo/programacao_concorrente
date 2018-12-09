57590e56b516d175bc745d0f8143f5f12618d576
José Glauber <jose.oliveira@ccc.ufcg.edu.br>
2018-12-09 10:51:30 -0300

    ideia da questão 3, ainda ta faltando estruturar, pois só está fazendo as chamadas, o que faz dar deadlock

---
+++ concorrente- LIsta 2/questao3.go
@@ -0,0 +1,79 @@
+package main
+
+import ("time"
+        "fmt"
+        "math/rand"
+)
+
+var ( canal1 = make(chan string)
+      canal2 = make(chan string)
+      canal3 = make(chan string)
+)
+
+func main() {
+    repetir()
+}
+
+func reliableRequest(){
+    
+    // Cada canal receberá um valor depois de uma certa quantidade
+    // de tempo, para simular e.x. o bloqueamento operações RPC
+    // executando em goroutines simultâneas.    
+    go func() {
+        r := rand.New(rand.NewSource(time.Now().UnixNano()))
+        var tempo = r.Intn(10)
+        time.Sleep(time.Second * time.Duration(tempo))
+        request("mirror1.com")
+        <- canal1
+    }()
+
+    go func() {
+        r := rand.New(rand.NewSource(time.Now().UnixNano()))
+        var tempo2 = r.Intn(10)
+        time.Sleep(time.Second * time.Duration(tempo2))
+        request("mirror2.br")
+        <- canal2
+    }()
+
+    go func(){
+        r := rand.New(rand.NewSource(time.Now().UnixNano()))
+        var tempo3 = r.Intn(10)
+        time.Sleep(time.Second * time.Duration(tempo3))
+        request("mirror3.edu")
+        <- canal3
+    }()
+
+
+    // Nós iremos utilizar o `select` para esperar esses valores
+    // simultâneamente, imprimindo cada um como ele chega.    
+    
+        select {
+          case msg1 := <-canal1:
+            fmt.Println("Primeiro servidor a responder", msg1)
+          case msg2 := <-canal2:
+            fmt.Println("Primeiro servidor a responder", msg2)
+          case msg3 := <-canal3:
+            fmt.Println("Primeiro servidor a responder", msg3)
+   }
+}
+
+func request(serverName string) {
+    
+    if serverName == "mirror1.com" {
+      canal1 <- "mirror1"
+    }
+
+    if serverName == "mirror2.br" {
+      canal2  <- "mirror2"
+    }
+
+    if serverName == "mirror3.edu" {
+      canal3 <- "mirror3"
+    }
+}
+
+func repetir() {
+  for {
+    reliableRequest()
+  }
+}
